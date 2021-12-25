561f8ef68aeedcedd2c8732946f07cb43a9ef088
DFoudi <david.foudi1@gmail.com>
2021-12-25 15:28:45 +0100

    Ajout du projet Pizza_Mama

---
+++ pizza_mama/Models/Pizza.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace pizza_mama.Models
+{
+    public class Pizza
+    {
+        [JsonIgnore]
+        public int PizzaID { get; set; }
+        [Display(Name = "Name")]
+        public string nom { get; set; }
+        [Display(Name = "Price (€)")]
+        public float prix { get; set; }
+        [Display(Name = "Vegetarian")]
+        public bool vegetarienne { get; set; }
+        [Display(Name = "Ingredients")]
+        [JsonIgnore]
+        public string ingredients { get; set; }
+
+        [NotMapped]
+        [JsonPropertyName ("ingredients")]
+        public string[] listeIngredients 
+        { 
+            get 
+            { 
+                if ((ingredients == null)||(ingredients.Count() == 0))
+                {
+                    return null;
+                }
+
+                return ingredients.Split(", ");
+            } 
+        }
+    }
+}
